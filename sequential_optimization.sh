d17240a6844a14fc54201e67e8f6a6549a9ede57
Anonymous <>
2019-12-13 15:54:14 -0500

    Update

--- sequential_optimization.sh
+++ sequential_optimization.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-python weight_virtualization.py -mode=t -vnn_name=network1 -iter=20000
-python weight_virtualization.py -mode=e -vnn_name=network1
-python weight_virtualization.py -mode=e -vnn_name=network2
-python weight_virtualization.py -mode=e -vnn_name=network3
-python weight_virtualization.py -mode=e -vnn_name=network4
-python weight_virtualization.py -mode=e -vnn_name=network5
+python weight_virtualization.py -mode=t -vnn_name=mnist -iter=20000
+python weight_virtualization.py -mode=e -vnn_name=mnist
+python weight_virtualization.py -mode=e -vnn_name=gsc
+python weight_virtualization.py -mode=e -vnn_name=gtsrb
+python weight_virtualization.py -mode=e -vnn_name=cifar10
+python weight_virtualization.py -mode=e -vnn_name=svhn
 
-python weight_virtualization.py -mode=t -vnn_name=network2 -iter=20000
-python weight_virtualization.py -mode=e -vnn_name=network1
-python weight_virtualization.py -mode=e -vnn_name=network2
-python weight_virtualization.py -mode=e -vnn_name=network3
-python weight_virtualization.py -mode=e -vnn_name=network4
-python weight_virtualization.py -mode=e -vnn_name=network5
+python weight_virtualization.py -mode=t -vnn_name=gsc -iter=20000
+python weight_virtualization.py -mode=e -vnn_name=mnist
+python weight_virtualization.py -mode=e -vnn_name=gsc
+python weight_virtualization.py -mode=e -vnn_name=gtsrb
+python weight_virtualization.py -mode=e -vnn_name=cifar10
+python weight_virtualization.py -mode=e -vnn_name=svhn
 
-python weight_virtualization.py -mode=t -vnn_name=network3 -iter=20000
-python weight_virtualization.py -mode=e -vnn_name=network1
-python weight_virtualization.py -mode=e -vnn_name=network2
-python weight_virtualization.py -mode=e -vnn_name=network3
-python weight_virtualization.py -mode=e -vnn_name=network4
-python weight_virtualization.py -mode=e -vnn_name=network5
+python weight_virtualization.py -mode=t -vnn_name=gtsrb -iter=20000
+python weight_virtualization.py -mode=e -vnn_name=mnist
+python weight_virtualization.py -mode=e -vnn_name=gsc
+python weight_virtualization.py -mode=e -vnn_name=gtsrb
+python weight_virtualization.py -mode=e -vnn_name=cifar10
+python weight_virtualization.py -mode=e -vnn_name=svhn
 
-python weight_virtualization.py -mode=t -vnn_name=network4 -iter=20000
-python weight_virtualization.py -mode=e -vnn_name=network1
-python weight_virtualization.py -mode=e -vnn_name=network2
-python weight_virtualization.py -mode=e -vnn_name=network3
-python weight_virtualization.py -mode=e -vnn_name=network4
-python weight_virtualization.py -mode=e -vnn_name=network5
+python weight_virtualization.py -mode=t -vnn_name=cifar10 -iter=20000
+python weight_virtualization.py -mode=e -vnn_name=mnist
+python weight_virtualization.py -mode=e -vnn_name=gsc
+python weight_virtualization.py -mode=e -vnn_name=gtsrb
+python weight_virtualization.py -mode=e -vnn_name=cifar10
+python weight_virtualization.py -mode=e -vnn_name=svhn
 
-python weight_virtualization.py -mode=t -vnn_name=network5 -iter=20000
-python weight_virtualization.py -mode=e -vnn_name=network1
-python weight_virtualization.py -mode=e -vnn_name=network2
-python weight_virtualization.py -mode=e -vnn_name=network3
-python weight_virtualization.py -mode=e -vnn_name=network4
-python weight_virtualization.py -mode=e -vnn_name=network5
+python weight_virtualization.py -mode=t -vnn_name=svhn -iter=20000
+python weight_virtualization.py -mode=e -vnn_name=mnist
+python weight_virtualization.py -mode=e -vnn_name=gsc
+python weight_virtualization.py -mode=e -vnn_name=gtsrb
+python weight_virtualization.py -mode=e -vnn_name=cifar10
+python weight_virtualization.py -mode=e -vnn_name=svhn
