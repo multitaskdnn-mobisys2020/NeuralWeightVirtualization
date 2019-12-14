e56f15ea377e15b23e7de0c465740cb09ae5d68f
Anonymous <multitaskdnn.mobisys2020@gmail.com>
2019-12-13 22:47:18 -0500

    Update

--- joint_optimization.sh
+++ joint_optimization.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in {1..10}
+for i in {1..20}
 do
-   	echo "$ith joint optimization"
+	echo "$i-th joint optimization"
 	python weight_virtualization.py -mode=t -vnn_name=mnist -iter=2000
 	python weight_virtualization.py -mode=e -vnn_name=mnist
 	python weight_virtualization.py -mode=e -vnn_name=gsc
